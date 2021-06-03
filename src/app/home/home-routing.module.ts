269d6cb7730e1917a5ae3abbd44d0070701190ab
Stefano Paulo <stefanopaulo47@gmail.com>
2021-06-03 17:20:16 -0300

    criando componente de usuario

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,3 +1,4 @@
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: '',
         component: LoginComponent
+      },
+      {
+        path: 'novousuario',
+        component: NovoUsuarioComponent
       }
     ]
   }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,10 +7,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
+  declarations: [HomeComponent, LoginComponent, NovoUsuarioComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
