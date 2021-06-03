fa601d087c5065f0fe3b28ca2c9b7c5e5e124af3
Stefano Paulo <stefanopaulo47@gmail.com>
2021-06-03 14:43:09 -0300

    consumindo o servico de autenticacao

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,14 +11,19 @@ export class LoginComponent implements OnInit {
   usuario = '';
   senha = '';
 
-  constructor() { }
+  constructor(private authService: AutenticacaoService) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    console.log(this.usuario);
-    console.log(this.senha);
+    this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
+      console.log('Autenticado com sucesso!');
+    },
+    error => {
+      alert('Usuário ou senha inválido');
+      console.log(error);
+    });
   }
 
 }
