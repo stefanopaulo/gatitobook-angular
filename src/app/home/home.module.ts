3ea8376b9a54dd1df68a941f54de8c150da1e3b2
Stefano Paulo <stefanopaulo47@gmail.com>
2021-06-03 14:33:00 -0300

    formulario template-driven

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule
   ],
   exports: [HomeComponent]
 })
