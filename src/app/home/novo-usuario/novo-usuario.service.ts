312c38afb793b1f7dbb00be2f716e5a2d1accdab
Stefano Paulo <stefanopaulo47@gmail.com>
2021-06-03 17:32:45 -0300

    servico de novo usuario

---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,16 @@
+import { NovoUsuario } from './novo-usuario';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+  cadastraNovoUsuario(novoUsuario: NovoUsuario): Observable<any> {
+    return this.http.post('http://localhost:3000/user/signup', novoUsuario);
+  }
+}
