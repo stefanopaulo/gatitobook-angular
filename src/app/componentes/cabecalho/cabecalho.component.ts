5c007c23fa42509e9647c516522108d043d049f2
Stefano Paulo <stefanopaulo47@gmail.com>
2021-06-10 16:35:51 -0300

    cabecalho

---
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -0,0 +1,21 @@
+import { Router } from '@angular/router';
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecalho',
+  templateUrl: './cabecalho.component.html',
+  styleUrls: ['./cabecalho.component.css']
+})
+export class CabecalhoComponent {
+
+  user$ = this.usuarioService.retornaUsuario();
+
+  constructor(private usuarioService: UsuarioService, private router: Router) { }
+
+  logout() {
+    this.usuarioService.logout();
+    this.router.navigate(['']);
+  }
+
+}
