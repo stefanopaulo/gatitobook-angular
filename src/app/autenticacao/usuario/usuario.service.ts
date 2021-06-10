c1f20c6728411c0a949e2ccfe233cee9c1e69b6a
Stefano Paulo <stefanopaulo47@gmail.com>
2021-06-10 16:15:25 -0300

    servico de usuario

---
+++ src/app/autenticacao/usuario/usuario.service.ts
@@ -0,0 +1,44 @@
+import { Router } from '@angular/router';
+import { Usuario } from './usuario';
+import { TokenService } from './../token.service';
+import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private usuarioSubject = new BehaviorSubject<Usuario>({});
+
+  constructor(private tokenService: TokenService, private router: Router) {
+    if (this.tokenService.possuiToken()) {
+      this.decodificaJWT();
+    }
+  }
+
+  private decodificaJWT() {
+    const token = this.tokenService.retornaToken();
+    const usuario = jwt_decode(token) as Usuario;
+    this.usuarioSubject.next(usuario);
+  }
+
+  retornaUsuario() {
+    return this.usuarioSubject.asObservable();
+  }
+
+  salvaToken(token: string) {
+    this.tokenService.salvaToken(token);
+    this.decodificaJWT();
+  }
+
+  logout() {
+    this.tokenService.excluiToken();
+    this.usuarioSubject.next({});
+  }
+
+  estaLogado() {
+    return this.tokenService.possuiToken();
+  }
+}
