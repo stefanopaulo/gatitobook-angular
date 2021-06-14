314ff1e86aa4f031516885d78e30c0a5063f3ee8
Stefano Paulo <stefanopaulo47@gmail.com>
2021-06-14 16:44:35 -0300

    servico animais

---
+++ src/app/animais/animais.service.ts
@@ -0,0 +1,23 @@
+import { TokenService } from './../autenticacao/token.service';
+import { Animais } from './animais';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const API = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimaisService {
+
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
+
+  listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-accees-token', token);
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
+      headers
+    });
+  }
+}
