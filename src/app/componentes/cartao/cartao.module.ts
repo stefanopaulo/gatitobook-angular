1d96388452eceb2cb4bd9da1c9cc64fb2ccf87b1
Stefano Paulo <stefanopaulo47@gmail.com>
2021-06-14 16:33:24 -0300

    componente de cartao reutilizavel

---
+++ src/app/componentes/cartao/cartao.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartaoComponent } from './cartao.component';
+
+
+
+@NgModule({
+  declarations: [CartaoComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [CartaoComponent]
+})
+export class CartaoModule { }
