c4b8841517778a4433abc97fc8e3d372d7aff618
Stefano Paulo <stefanopaulo47@gmail.com>
2021-06-28 16:22:08 -0300

    lista de animais

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,3 +1,6 @@
+import { AnimaisService } from './../animais.service';
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { Animais } from './../animais';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  constructor() { }
+  animais !: Animais;
+
+  constructor(private usuarioService: UsuarioService, private animaisService: AnimaisService) { }
 
   ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+      const userName = usuario.name ?? '';
+      this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+        this.animais = animais;
+      });
+    });
   }
 
 }
