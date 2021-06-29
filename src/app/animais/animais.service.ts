79f8a9fa89ce02f3f0107f80cc6cbb51c12e6358
Stefano Paulo <stefanopaulo47@gmail.com>
2021-06-29 16:26:45 -0300

    metodos de curtir e excluir foto

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,11 +1,13 @@
 import { environment } from './../../environments/environment';
 import { TokenService } from './../autenticacao/token.service';
 import { Animais, Animal } from './animais';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, mapTo } from 'rxjs/operators';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +22,20 @@ export class AnimaisService {
 
   buscaPorId(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  excluiAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: number): Observable<boolean> {
+    return this.http.post(`${API}/photos/${id}/likes`, {}, {
+      observe: 'response'
+    }).pipe(
+      mapTo(true),
+      catchError((error) => {
+        return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+      })
+    );
   }
 }
