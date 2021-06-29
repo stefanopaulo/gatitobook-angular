02acf0a957c74a1dc6f5d9905407b97d463ec642
Stefano Paulo <stefanopaulo47@gmail.com>
2021-06-29 17:40:09 -0300

    guarda resover

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -2,11 +2,15 @@ import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.componen
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
 
 const routes: Routes = [
   {
     path: '',
-    component: ListaAnimaisComponent
+    component: ListaAnimaisComponent,
+    resolve: {
+      animais: ListaAnimaisResolver
+    }
   },
   {
     path: ':animalId',
--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -4,6 +4,7 @@ import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
 import { Animais } from './../animais';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lista-animais',
@@ -12,17 +13,14 @@ import { Observable } from 'rxjs';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  animais$ !: Observable<Animais>;
+  animais !: Animais;
 
-  constructor(private usuarioService: UsuarioService, private animaisService: AnimaisService) { }
+  constructor(private activateRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.animais$ = this.usuarioService.retornaUsuario().pipe(
-      switchMap((usuario) => {
-        const userName = usuario.name ?? '';
-        return this.animaisService.listaDoUsuario(userName);
-      })
-    );
+    this.activateRoute.params.subscribe(params => {
+      this.animais = this.activateRoute.snapshot.data['animais'];
+    });
   }
 
 }
